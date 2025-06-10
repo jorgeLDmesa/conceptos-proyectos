483c1025dd59a3c7a956f9b3204f9957be65c111
jorge.david1 <jorge.david1@udea.edu.co>
2025-06-09 19:08:19 -0500

    button login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import Image from "next/image"
 import Link from "next/link"
 
